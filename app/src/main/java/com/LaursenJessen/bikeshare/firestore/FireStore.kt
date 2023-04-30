ee05201c9c1942797a2f046cf4a452d323e8ea4d
Thomas <201905236@post.au.dk>
2023-04-30 22:07:22 +0200

    minor bug fix

--- app/src/main/java/com/LaursenJessen/bikeshare/firestore/FireStore.kt
+++ app/src/main/java/com/LaursenJessen/bikeshare/firestore/FireStore.kt
@@ -48,7 +48,6 @@ class FireStore(private val storage: FirebaseStorage, private val api: FirebaseF
                         val user = auth.currentUser ?: throw Exception("Something wrong")
                         val signedInUser = user.email?.let { User(user.providerId, it) }
                             ?: throw Exception("createUserWithEmail:$email failure")
-                        isLoggedInChanged(true)
                         continuation.resume(signedInUser)
                     } else {
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
@@ -57,27 +56,21 @@ class FireStore(private val storage: FirebaseStorage, private val api: FirebaseF
                 }
         }
     }
-    suspend fun login(email: String, password: String): User {
-        try {
-            val result = auth.signInWithEmailAndPassword(email, password).await()
-            val user = result.user ?: throw Exception("Something went wrong")
-            val signedInUser = user.email?.let { User(user.providerId, it) }
-                ?: throw Exception("createUserWithEmail:$email failure")
-            isLoggedInChanged(true)
-            return signedInUser
-        } catch (e: FirebaseAuthInvalidCredentialsException) {
-            Log.w(TAG, "Invalid email or password", e)
-            throw Exception("Invalid email or password", e)
-        } catch (e: FirebaseAuthInvalidUserException) {
-            Log.w(TAG, "Invalid user", e)
-            throw Exception("Invalid user", e)
-        } catch (e: FirebaseTooManyRequestsException) {
-            Log.w(TAG, "All requests from this device is blocked due to unusual activity. Try again later", e)
-            throw Exception("All requests from this device is blocked due to unusual activity. Try again later", e)
-        }
-        catch (e: Exception) {
-            Log.w(TAG, "login failed", e)
-            throw Exception("login failed", e)
+    suspend fun login(email: String, password: String) {
+        suspendCoroutine { continuation ->
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser ?: throw Exception("Something wrong")
+                        val signedInUser = user.email?.let { User(user.providerId, it) }
+                            ?: throw Exception("createUserWithEmail:$email failure")
+                        continuation.resume(signedInUser)
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        throw throw Exception("createUserWithEmail: $email failure", task.exception)
+                    }
+                }
         }
     }
 
